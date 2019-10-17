a018a990bcc97c2cb96651cfab6d5c8571d3ae84
TgJe <51323906+tangjie133@users.noreply.github.com>
2019-10-17 10:15:53 +0800

    Update main.blocks, main.ts, colorsensor.ts

--- colorsensor.ts
+++ colorsensor.ts
@@ -1,10 +1,10 @@
-// Add your code here
+
 //%weight=100 color=#00A654  block="Colorsensor"
 namespace DFRobot_ColorSensor{
     let TCS34725_ADDRESS              =  0X29
     let REG_TCS34725_ID               =  0X12
     let REG_TCS34725_COMMAND_BIT      =  0X80
-    let TCS34725_BEGIN                =  0
+   
 
     let REG_TCS34725_GAIN             =  0X0F
     let REG_TCS34725_ATIME            =  0X01
@@ -21,7 +21,8 @@ namespace DFRobot_ColorSensor{
     let TCS34725_RGBC_R               =  0
     let TCS34725_RGBC_G               =  0
     let TCS34725_RGBC_B               =  0     
-   
+    let TCS34725_BEGIN                =  0
+
     function ReadReg(addr:number,reg:number):number {
         pins.i2cWriteNumber(addr, reg, NumberFormat.Int8LE);
         return pins.i2cReadNumber(addr, NumberFormat.Int8LE);
@@ -34,9 +35,9 @@ namespace DFRobot_ColorSensor{
         pins.i2cWriteBuffer(addr, buf);
     }
 
-    function GetInt16LE(addr:number,reg:number):number {
-        pins.i2cWriteNumber(addr, reg, NumberFormat.Int8LE)
-        return pins.i2cReadNumber(addr, NumberFormat.Int8LE)
+    function GetUInt16LE(addr:number,reg:number):number {
+        pins.i2cWriteNumber(addr, reg, NumberFormat.UInt8LE)
+        return pins.i2cReadNumber(addr, NumberFormat.UInt16LE)
     }
     /**
      * TCS34725 Color Sensor Init
@@ -56,10 +57,10 @@ namespace DFRobot_ColorSensor{
 
     function GetRGBC() {
         if(!TCS34725_BEGIN) Tcs34725Beging();
-        TCS34725_RGBC_C = GetInt16LE(TCS34725_ADDRESS, REG_CLEAR_CHANNEL_L | REG_TCS34725_COMMAND_BIT);
-        TCS34725_RGBC_R = GetInt16LE(TCS34725_ADDRESS, REG_RED_CHANNEL_L | REG_TCS34725_COMMAND_BIT);
-        TCS34725_RGBC_G = GetInt16LE(TCS34725_ADDRESS, REG_GREEN_CHANNEL_L | REG_TCS34725_COMMAND_BIT);
-        TCS34725_RGBC_B = GetInt16LE(TCS34725_ADDRESS, REG_BLUE_CHANNEL_L |REG_TCS34725_COMMAND_BIT );
+        TCS34725_RGBC_C = GetUInt16LE(TCS34725_ADDRESS, REG_CLEAR_CHANNEL_L | REG_TCS34725_COMMAND_BIT);
+        TCS34725_RGBC_R = GetUInt16LE(TCS34725_ADDRESS, REG_RED_CHANNEL_L | REG_TCS34725_COMMAND_BIT);
+        TCS34725_RGBC_G = GetUInt16LE(TCS34725_ADDRESS, REG_GREEN_CHANNEL_L | REG_TCS34725_COMMAND_BIT);
+        TCS34725_RGBC_B = GetUInt16LE(TCS34725_ADDRESS, REG_BLUE_CHANNEL_L |REG_TCS34725_COMMAND_BIT );
 
         basic.pause(50);
         let ret = ReadReg(TCS34725_ADDRESS, REG_TCS34725_ENABLE | REG_TCS34725_COMMAND_BIT);
@@ -72,7 +73,26 @@ namespace DFRobot_ColorSensor{
     //%block="get read"
     export function GetRead():number {
         GetRGBC();
-        return Math.round((Math.round(TCS34725_RGBC_R)/Math.round(TCS34725_RGBC_C))*255)
+        let R = (Math.round(TCS34725_RGBC_R) / Math.round(TCS34725_RGBC_C)) * 255
+        //serial.writeNumber(Math.round(R))
+        return R
+    }
+    //%block = "get green"
+    export function GetGreen():number {
+        GetRGBC();
+        let G = (Math.round(TCS34725_RGBC_G) / Math.round(TCS34725_RGBC_C)) * 255
+        return G
+    }
+    //%block = "get blue"
+    export function GetBlue() :number{
+        GetRGBC();
+        let B = (Math.round(TCS34725_RGBC_B) / Math.round(TCS34725_RGBC_C)) * 255
+        return B
+    }
+    //%block ="get light"
+    export function GetLinght():number {
+        GetRGBC();
+       return (Math.round(TCS34725_RGBC_C))  
     }
     
 }
--- main.ts
+++ main.ts
@@ -1,3 +1,3 @@
 basic.forever(function () {
-    basic.showNumber(0)
+    basic.showNumber(DFRobot_ColorSensor.GetRead())
 })
